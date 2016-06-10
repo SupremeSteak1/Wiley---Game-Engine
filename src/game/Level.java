4837fe592a3abaa583b0564795d407f4c1e6ae01
Thomas Hayden <supremesteak1@gmail.com>
2016-06-10 11:03:22 -0400

    Javadoced Level

--- src/game/Level.java
+++ src/game/Level.java
@@ -3,7 +3,12 @@ package game;
 public class Level {
 	
 	//Not implemented yet
-	public static Level loadLevel(String filePath){
+	/**
+	 * Loads a level from a file
+	 * @param filePath the file to load from
+	 * @return the loaded Level
+	 */
+	public static Level loadLevel(String filePath) {
 		return null;
 	}
 
