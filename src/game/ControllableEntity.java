ae5a41cb44f9cb8fdb508a71e10d8de567661dd2
Thomas Hayden <supremesteak1@gmail.com>
2016-06-10 11:37:40 -0400

    Javadoced ControllableEntity

--- src/game/ControllableEntity.java
+++ src/game/ControllableEntity.java
@@ -7,54 +7,84 @@ import engine.physics.RigidBody;
 import engine.physics.Vector;
 import other.Utilities;
 
-public class ControllableEntity extends RigidBody{
+public class ControllableEntity extends RigidBody {
 	
 	Vector velocity = super.getVelocity();
 	private double speed;
 	private double projectileSpeed;
 	
+	/**
+	 * Constructs a new ControllableEntity
+	 * @param x the x coordinate of the ControllableEntity
+	 * @param y the y coordinate of the ControllableEntity
+	 * @param m the mass of the ControllableEntity
+	 * @param xLength the length of the ControllableEntity on the x axis
+	 * @param yLength the length of the ControllableEntity on the y axis
+	 */
 	public ControllableEntity(int x, int y, int m, int xLength, int yLength) {
 		super(x, y, m, xLength, yLength);
 		this.speed = 1;
 		this.projectileSpeed = 1;
 	}
 	
+	/**
+	 * Gets the projectile's speed
+	 * @return the projectile's speed
+	 */
 	public double getProjectileSpeed() {
 		return projectileSpeed;
 	}
 
+	/**
+	 * Sets the projectile's speed
+	 * @param projectileSpeed the new speed
+	 */
 	public void setProjectileSpeed(double projectileSpeed) {
 		this.projectileSpeed = projectileSpeed;
 	}
-
-	public void setSpeed(double value){
+	
+	/**
+	 * Sets the ControllableEntity's speed
+	 * @param value the new speed
+	 */
+	public void setSpeed(double value) {
 		this.speed = value;
 	}
 	
-	public double getSpeed(){
+	/**
+	 * Gets the ControllableEntity's speed
+	 * @return this ControllableEntity's speed
+	 */
+	public double getSpeed() {
 		return this.speed;
 	}
 	
+	/**
+	 * The move method inherited from RigidBody
+	 */
 	@Override
-	public void move(){
-		if(Keyboard.isKeyPressed('w')){
+	public void move() {
+		if(Keyboard.isKeyPressed('w')) {
 			velocity = Utilities.addVectors(velocity, new Vector(0,speed));
 		}
-		if(Keyboard.isKeyPressed('s')){
+		if(Keyboard.isKeyPressed('s')) {
 			velocity = Utilities.addVectors(velocity, new Vector(0,-speed));
 		}
-		if(Keyboard.isKeyPressed('d')){
+		if(Keyboard.isKeyPressed('d')) {
 			velocity = Utilities.addVectors(velocity, new Vector(speed,0));
 		}
-		if(Keyboard.isKeyPressed('a')){
+		if(Keyboard.isKeyPressed('a')) {
 			velocity = Utilities.addVectors(velocity, new Vector(-speed,0));
-		}
-		else{
+		} else {
 			velocity = new Vector (0,0);
 		}
 		super.setVelocity(velocity);
 	}
 	
+	/**
+	 * Runs this ControllableEntity's directed actions
+	 * @param p the point they are directed towards
+	 */
 	public void directedAction(Point p){
 		Vector direction = Utilities.subtractVectors(new Vector(p.x, p.y), super.getPosition()).normalize();
 		//Default action:
